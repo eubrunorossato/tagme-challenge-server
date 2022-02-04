6f06d20b619df8bd7a3c7475bc5ef5b1a8ab611f
eubrunorossato <Bruno.Araujo1@ibm.com>
2022-02-04 14:40:30 -0300

    set up middleware order

--- src/middleware/order.js
+++ src/middleware/order.js
@@ -3,12 +3,12 @@ export default {
     const { itemId, step } = req.body;
 
     if (!itemId || itemId === null || itemId === '') {
-      res.status(500).json({
+      return res.status(500).json({
         message: 'Item id must be specified.',
       });
     }
     if (!step || step === null || step === '') {
-      res.status(500).json({
+      return res.status(500).json({
         message: 'Step must be specified.',
       });
     }
