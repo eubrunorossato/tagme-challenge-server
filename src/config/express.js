88a971470570e99b26f353623ad917f4c157fec8
eubrunorossato <Bruno.Araujo1@ibm.com>
2022-02-07 22:21:01 -0300

    set up communication front-back

--- src/config/express.js
+++ src/config/express.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from '../router';
-import helmet from 'helmet'
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(morgan(':url :method'));
+app.use(
+  cors({
+    origin: 'http://localhost:3001',
+    optionsSuccessStatus: 200,
+  })
+);
+app.use(helmet());
 app.use(routes);
-app.use(helmet())
 
 export default app;
